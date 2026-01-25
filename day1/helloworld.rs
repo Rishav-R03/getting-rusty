eace7c3222b17f566d9e4c82881f66a24c6fdb4a
Rishav-R03 <rishav042023@gmail.com>
2026-01-25 23:16:09 +0530

    getting started with rust

---
+++ day1/helloworld.rs
@@ -0,0 +1,13 @@
+fn main() {
+    //println! calls rust macro.
+    //without ! it will be calling a method called println
+    //so ! is important
+
+    //macros are way to write code that generates code to extend
+    //rust syntax
+
+    println!("Hello World");
+
+    //before running compile program using rustc <>.rs
+    
+}
